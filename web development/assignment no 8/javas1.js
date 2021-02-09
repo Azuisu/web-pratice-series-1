01f4aeb88d29a8df91d7e674dfa2001cf6a9e25b
Azuisu <ishfaq1001@yahoo.com>
2021-02-09 11:40:29 +0530

    new files

---
+++ web development/assignment no 8/javas1.js
@@ -0,0 +1,7 @@
+document.write("<h1> welcome to javascript </h1> ");
+document.write("<h2> hello world </h2>");
+document.write(" <h3> azmat ishfaq </h3> ");
+document.write("<p> hi this is azmat ishfaq from tangmarg kashmir </p>")
+
+
+
